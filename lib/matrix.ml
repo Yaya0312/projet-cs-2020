78a6776a51d1a4d530834b715f27c79690c29ed5
2¥ <yahiyanis@gmail.com>
2020-03-09 14:51:14 +0100

    save

--- lib/matrix.ml
+++ lib/matrix.ml
@@ -2,38 +2,59 @@ type matrix = float array array;;
 (* TODO demander prof pour rendre generique*)
 
 let get_row (m:matrix) (row:int) : (float array) = 
-    Array.get m row
+  Array.get m row
 ;;
 
 let get_col (m:matrix) (col:int) : (float array) = 
-    Array.map (fun m -> Array.get m col) m
+  Array.map (fun m -> Array.get m col) m
+;;
+
+let is_squared (m:matrix) : bool =
+  (Array.length m) = (Array.length m.(0))
 ;;
 
 let get_diag (m:matrix) : (float array) =
-    let pos = ref (-1) in
-    Array.map (fun m -> incr pos; Array.get m !pos) m
+  let pos = ref (-1) in
+  Array.map (fun m -> incr pos; Array.get m !pos) m
 ;;
 
 let mul_row (m:matrix) (row:int) (coef:float) : unit =
-   Array.set m row (Array.map (fun n -> n *. coef) m.(row))
+  Array.set m row (Array.map (fun n -> n *. coef) m.(row))
 ;;
 
 let sum_row (m:matrix) (num_row:int) (row:'a array) : unit =
-   Array.set m row (Array.map (fun n -> n *. coef) m.(row))
+  Array.set m row (Array.map (fun n -> n *. coef) m.(row))
 ;;
 
 let swap_row (m:matrix) (row1:int) (row2:int) : unit =
-    let tmp = m.(row1) in
-    m.(row1) <- m.(row2);
-    m.(row2) <- tmp
+  let tmp = m.(row1) in
+  m.(row1) <- m.(row2);
+  m.(row2) <- tmp
 ;;
 
-let max_pos_array (a:float array) : int = (* TODO *)
-  let pos = ref (min_float, 0, -1) (* val_max, current_pos, max_pos *)
-  Array.iter (fun e ->  ) a
-
+let max_pos_array (a:float array) = (* TODO *)
+  let max_pos = ref 0;
+    let current_pos = ref (-1);
+      let f (e:float) : unit = 
+        incr current_pos;
+        (if ((Float.abs e) > (Array.get a !current_pos)) then 
+           max_pos := !current_pos);
+        ()
+        ;
+        Array.iter f a
 ;;
 
-let solve (m:matrix) (row:int) : int = 
-  
+let max_v2 (b:float array) : int = 
+  let rec aux (a:float array) (e_max:float) (p_max:int) (pos:int): int =
+    if (pos = Array.length a) then
+      p_max
+    else if (e_max < Float.abs(Array.get a pos)) then
+      aux a (Array.get a pos) pos (pos+1)
+    else 
+      aux a e_max p_max (pos+1)
+  in aux b (Array.get b 0) 0 0
+;;
+
+let solve (m:matrix) (row:float array) : float array = 
+
 ;;
--- lib/matrix.mli
+++ lib/matrix.mli
@@ -1,38 +1,50 @@
 type matrix
 
 (** 
-    [get_row m i] Retourne la ligne i de la matrice m 
+    [get_row m i]
+    Retourne la ligne i de la matrice m 
     Complexity O(1)
 *)
 val get_row : matrix -> int -> float array (** TODO replace par 'a *)
 
 (** 
-    [get_col m i] Retourne la colone i de la matrice m
+    [get_col m i]
+    Retourne la colone i de la matrice m
     Complexity O(n) n étant le nombre de lignes
 *)
 val get_col : matrix -> int -> float array (** TODO replace par 'a *)
 
 (** 
-    [get_diag m] Retourne la diagonale de la matrice carré m
+    [get_diag m]
+    Retourne la diagonale de la matrice carré m
     Complexity O(n) n étant le nombre de lignes
 *)
 val get_diag : matrix -> float array (** TODO replace par 'a *)
 
 (** 
-    [mul_row m row coef] Multiplie la ligne row de la matrice m par coeff
+    [mul_row m row coef]
+    Multiplie la ligne row de la matrice m par coeff
     Complexity O(n) TODO
 *)
 val mul_row : matrix -> int -> float -> unit
 
 (** 
-    [sum_row m num_row row] Additionne la ligne num_row de la matrice m avec
-    la ligne row
+    [sum_row m num_row row]
+    Additionne la ligne num_row de la matrice m avec la ligne row
     Complexity O(n) TODO
 *)
 val sum_row : matrix -> int -> 'a array -> unit
 
 (** 
-    [swap_row m row1 row2] Échange les lignes row1 et row2 de la matrice m
+    [swap_row m row1 row2]
+    Échange les lignes row1 et row2 de la matrice m
     Complexity O(n) TODO
 *)
 val swap_row : matrix -> int -> int -> unit
+
+(**
+    [solve m row]
+    Retourne l'ensemble des solutions en utilisant l'algorithme de gauss
+    Complexity ??
+*)
+val solve : matrix -> float array -> float array
