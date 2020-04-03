8a4871bc36ae154bef9d6f4d6023dacd24805a97
2¥ <yahiyanis@gmail.com>
2020-04-03 10:04:43 +0200

    add const

--- main.ml
+++ main.ml
@@ -1,8 +1,10 @@
 open BenchTools;;
 
-let t1 = make_array 0 10000;;
+let sample = 200;;
+let maxDeg = 10_000;;
+let t1 = make_array 0 maxDeg;;
 shuffle t1 ~start:10 ();;
-let t1 = Array.sub t1 0 1000;;
-let ht = Hashtbl.create 1000;;
+let t1 = Array.sub t1 0 sample;;
+let ht = Hashtbl.create ~random:false sample;;
 Array.iter (fun a -> Hashtbl.add ht a (calc a)) t1;;
 export_latex ht "mult.dat";;
