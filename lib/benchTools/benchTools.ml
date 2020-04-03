d3b1b14e61f365dbf524e734d8ce5313cf0bfc48
2¥ <yahiyanis@gmail.com>
2020-04-03 10:06:28 +0200

    opti code && add doc

--- lib/benchTools/benchTools.ml
+++ lib/benchTools/benchTools.ml
@@ -8,7 +8,8 @@ let swap (a:'a array) (i:int) (j:int) : unit =
   a.(j) <- t
 ;;
 
-let shuffle (a:'a array) ?(start = 0) ?(len = Array.length a) () : unit =
+let shuffle (a:'a array) ?(start = 0) ?(len = (Array.length a)-start) () 
+  : unit =
   Array.iteri (fun i _ -> swap a i (Random.int (i + 1))) (Array.sub a start len)
 ;;
 
@@ -18,55 +19,43 @@ let make_array (min:int) (max:int) : int array =
     Array.init (r + 1) (fun i -> (i + min))
 ;;
 
-(** END TOOLS *)
-
-let create_array_polynome (num:int) (deg:int) = 
-  Array.init (num + 1) (fun _ -> random_poly deg Base.Float.max_value)
+(** 
+   [time_fun func arg] retourne le temps que la fonction fun à mis pour 
+   s'executer
+*)
+let time_fun func =
+  let start_time = Sys.time () in
+  ignore (func);
+  Sys.time () -. start_time
 ;;
 
+(*** END TOOLS ****************************************************************)
+
 (** 
-   Pioche deux element dans la liste
+   [pick_double a] pioche aléatoirement deux elements dans la liste a \n
    Complexité O(1)
 *)
-
-let pick a =
-  let random1 = Random.int (Array.length a) and
-  random2 = Random.int (Array.length a) in
-  a.(random1), a.(random2)
-;;
-
-(** Execute la fonction iter fois et retourne la moyenne *)
-(* let average (iter:int) f : float =
-   let rec aux acc pos = 
-    if (pos = 0) then
-      acc /. (float_of_int iter)
-    else 
-      aux (acc +. f) (pos + 1)
-   in aux 0. iter
-   ;; *)
-
-(** 
-   Retourne le temps que la fonction à mis pour s'executer
-*)
-let time_fun func arg =
-  let start_time = Sys.time () in
-  ignore (func arg);
-  let finish_time = Sys.time () in
-  finish_time -. start_time
+let pick_double a =
+  let pick l = a.(Random.int (Array.length l)) in
+  (pick a),(pick a)
 ;;
 
 let export_latex ht (file:string) : unit = 
-  let oc = open_out file in  (* create or truncate file, return channel *)
-  Printf.fprintf oc "# Génération : %f (unix time) \n" (Unix.time ());
-  Hashtbl.iter (fun k v -> Printf.fprintf oc "%d %f \n" k v.(0)  ) ht;
+  let oc = open_out file in
+  Printf.fprintf oc "# Génération temps multiplication \n";
+  Printf.fprintf oc "P $Naive$ $Kara$ $Toom3$ \n";
+  Hashtbl.iter (fun k v -> Printf.fprintf oc 
+                   "%d %f %f %f \n" 
+                   k v.(0) v.(1) v.(2))
+    ht;
   close_out oc;
 ;;
 
 let calc (deg:int) : (float array) =
-  let pList = create_array_polynome 10 deg in
-  let p1,p2 = pick pList in 
-  (* let tk = time_fun (fun _ -> karatsuba  p1 p2) () in *)
-  let tn = time_fun (fun _ -> mult_naive p1 p2) () in 
-  (* let tc = time_fun (fun _ -> Polynome.toom_cook3 p1 p2) () in *)
-  [|tn|]
+  let p1,p2 = (Array.init 11 (fun _ -> random_poly deg ()) ) |> pick_double in 
+  [|
+    time_fun (fun _ -> mult_naive p1 p2);
+    time_fun (fun _ -> karatsuba  p1 p2);
+    time_fun (fun _ -> toom_cook3 p1 p2);
+  |]
 ;;
--- lib/benchTools/benchTools.mli
+++ lib/benchTools/benchTools.mli
@@ -1,13 +1,11 @@
-exception MaxLowerThanMin;;
-
 (** 
-    Interverti les valeurs a[i] a[j] du tableau
+    [swap a i j] Interverti les valeurs a[i] a[j] du tableau a \n
     Complexité O(1)
 *)
 val swap : 'a array -> int -> int -> unit
 
 (** 
-    Mélange le tableau
+    [shuffle a start ] Mélange le tableau a \n
     Complexité O(n) -  Parcours le tableau de longueur n
 *)
 val shuffle : 'a array -> ?start:int -> ?len:int -> unit -> unit
@@ -17,6 +15,7 @@ val shuffle : 'a array -> ?start:int -> ?len:int -> unit -> unit
     Complexity O(n) - Initialise le tableau de longueur n
 *)
 val make_array : int -> int -> int array
+
 
 val calc: int -> float array
 
